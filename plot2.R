d98c43278755a89c75e8b79d16c70dc34436c74f
davidberkowitz <davidberkowitz2009@gmail.com>
2014-10-09 23:27:21 -0700

    Initial check-ins

---
+++ plot2.R
@@ -0,0 +1,49 @@
+
+createPlot2 <- function() 
+{
+	cat ("outcomes\n")
+	## read in the complete data file
+	## separator is ";" so use read.csv2
+	outcomes <- read.csv2 ("household_power_consumption.txt", colClasses = "character")
+	str (outcomes)
+	
+	# having some problems with dates,
+	# so brute forcing it for now
+	
+	cat ("dayOne\n")
+	dayOne = subset (outcomes, outcomes$Date=="1/2/2007")
+	str (dayOne)
+	
+	cat ("dayTwo\n")	
+	dayTwo = subset (outcomes, outcomes$Date=="2/2/2007")
+	str (dayTwo)
+
+	# combine them into a single data frame
+	cat ("data\n")	
+	data = rbind(dayOne, dayTwo)
+	str (data)
+
+	## convert the column of interest to numeric
+	cat ("Global_active_power\n")	
+	data$Global_active_power = as.numeric (data$Global_active_power)
+	str (data)
+	
+	## create a new DateTime column
+	data$DateTime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
+		
+	## open the graphics device
+	png (filename = "plot2.png")
+	
+	## setup the graphics parameters
+	par (bg = "transparent")
+	
+	## create the line plot
+	plot (data$DateTime,
+		data$Global_active_power, 
+		type = "l",
+		ylab="Global Active Power (kilowatts)",
+    xlab="")
+
+	## close the graphics device (which saves the PNG)
+	dev.off()
+}
---
+++ plot3.R
@@ -0,0 +1,60 @@
+
+createPlot3 <- function() 
+{
+	cat ("outcomes\n")
+	## read in the complete data file
+	## separator is ";" so use read.csv2
+	outcomes <- read.csv2 ("household_power_consumption.txt", colClasses = "character")
+	str (outcomes)
+	
+	# having some problems with dates,
+	# so brute forcing it for now
+	
+	cat ("dayOne\n")
+	dayOne = subset (outcomes, outcomes$Date=="1/2/2007")
+	str (dayOne)
+	
+	cat ("dayTwo\n")	
+	dayTwo = subset (outcomes, outcomes$Date=="2/2/2007")
+	str (dayTwo)
+
+	# combine them into a single data frame
+	cat ("data\n")	
+	data = rbind(dayOne, dayTwo)
+	str (data)
+
+	## convert the columns of interest to numeric
+	cat ("Sub_metering\n")	
+	data$Sub_metering_1 = as.numeric (data$Sub_metering_1)
+	data$Sub_metering_2 = as.numeric (data$Sub_metering_2)
+	data$Sub_metering_3 = as.numeric (data$Sub_metering_3)
+	str (data)
+	
+	## create a new DateTime column
+	data$DateTime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
+		
+	## open the graphics device
+	png (filename = "plot3.png")
+	
+	## setup the graphics parameters
+	par (bg = "transparent")
+	
+	## create the plot frame
+	plot (data$DateTime,
+		data$Sub_metering_1, 
+		type = "n",
+		ylab="Energy sub metering",
+    xlab="")
+
+  ## draw the lines
+	lines (data$DateTime, data$Sub_metering_1, col="black")
+	lines (data$DateTime, data$Sub_metering_2, col="red")
+	lines (data$DateTime, data$Sub_metering_3, col="blue")
+  
+  ## make the legend
+  # wrong pch for now
+	legend ("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), , lwd=c(2.5,2.5,2.5))
+  
+	## close the graphics device (which saves the PNG)
+	dev.off()
+}
